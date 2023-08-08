7e25ba1592728892f05969f9c3a2894f673b96b6
Oburjin <140142233+Oburjin@users.noreply.github.com>
2023-08-08 12:31:07 +1000

    Solution

--- palindrome.py
+++ palindrome.py
@@ -20,3 +20,60 @@ def check_palindrome(input_str: str) -> bool:
     
     :Complexity: O(n) where n is the length of the input string
     """
+
+    # array = ArrayStack(len(input_str)//2+1)
+    # for i in range(len(input_str)//2+1):
+    #     array.push(input_str[i])
+    # for i in range(len(array.length)):
+    #     if array.pop == array.
+
+    # array = ArrayStack(len(input_str))
+    # for i in range(len(input_str)):
+    #     array.length += 1
+    #     array.push(input_str[i])
+    # for i in range(len(array.length)):
+    #     temp = array.pop()
+    #     array.length -= 1
+    #     if temp == array.peek():
+    #         for i in range(len(array.length)):
+    
+    # array = ArrayStack()
+    # for i in range(len(input_str)):
+    #     array.length += 1
+    #     array.push(input_str[i])
+    # back_array = ArrayStack()
+    # for i in range(len(array.length)):
+    #     back_array.length += 1
+    #     back_array.push(array.pop)
+    #     array.length -= 1
+    #     if back_array.peek() != array.pop():
+    #         return False
+    # return True
+
+    # LESS OPTIMISED
+    # array = ArrayStack()
+    # for i in range(len(input_str)):
+    #     array.length += 1
+    #     array.push(input_str[i])
+    #     if array.peek() != input_str[i]:
+    #         return False
+    # return True
+
+    # # MORE OPTIMISED
+    # array = ArrayStack(len(input_str))
+    # for i in range(len(input_str)):
+    #     array.length += 1
+    #     array.push(input_str[i])
+    # for i in range(len(input_str//2)):
+    #     if array.peek() != input_str[i]:
+    #         return False
+    # return True
+
+    # MORE OPTIMISED
+    array = ArrayStack(len(input_str))
+    for i in range(len(input_str)):
+        array.push(input_str[i])
+    for i in range(len(input_str)//2):
+        if array.peek() != input_str[i]:
+            return False
+    return True
